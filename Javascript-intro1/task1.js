d2548a3dc02963be05288d96f09dc30f4ba133b5
Aziz Imastara <sazizimastara@gmail.com>
2021-09-07 20:38:16 +0700

    first commit

---
+++ Javascript-intro1/task1.js
@@ -0,0 +1,39 @@
+let biodata = {
+    name : 'Moh. Syahrul Aziz Imastara',
+    age : 19,
+    hobbies : ['Basket', 'Gaming'],
+    isMarried : false,
+    schoolList: [
+        {
+            name: "SDN Paseyan 1",
+            yearIn: 2008,
+            yearOut: 2014,
+            major: null
+        },
+        {
+            name: "SMP Negeri 1 Jatirogo",
+            yearIn: 2014,
+            yearOut: 2017,
+            major: null
+        },
+        {
+            name: "SMA Negeri Jatirogo",
+            yearIn: 2017,
+            yearOut: 2020,
+            major: "IPA"
+        },
+    ],
+    skill : [
+        {
+            skillName: 'CSS',
+            level: 'beginner',
+        },
+        {
+            skillName: 'Javascript',
+            level: 'beginner',
+        },
+    ],
+    interestInCoding: true
+};
+
+console.log(biodata)
