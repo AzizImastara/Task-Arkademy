a1b0968e6f2f59c40412f68a3ea4467ba89e662f
Aziz Imastara <sazizimastara@gmail.com>
2021-09-09 05:22:56 +0700

    Javascript intro 1

--- Javascript-intro1/task3.js
+++ Javascript-intro1/task3.js
@@ -1,9 +1,14 @@
 const num = 5;
 
+if (typeof num === 'number' && num > 0) {
   for (let i = num; i >= 1; i--) {
-    let spc = ''
+    let cetakSegitiga = ''
     for (let j = 1; j <= i; j++){
-      spc += j    
+      cetakSegitiga += j    
     }
-    console.log(spc);
+    console.log(cetakSegitiga);
   }
+} 
+else {
+  console.log('Data harus number');
+}
