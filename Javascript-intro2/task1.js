7a335cf374fe522ba0c23baa3a4266146adc4758
Aziz Imastara <sazizimastara@gmail.com>
2021-09-10 20:23:19 +0700

    Javascript intro 2

---
+++ Javascript-intro2/task1.js
@@ -0,0 +1,39 @@
+// 10 Method Javascript
+
+// 1. toLowerCase() digunakan untuk mengubah semua karakter menjadi huruf kecil
+const str = 'HELLO';
+const low = str.toLowerCase();
+console.log(low);
+
+// 2. toUpperCase() digunakan untuk mengubah semua karakter menjadi huruf besar
+const src = 'hello';
+const up = src.toUpperCase();
+console.log(up);
+
+// 3. charAt() digunakan untuk mengembalikan karakter pada indeks tertentu dalam sebuah string
+const char = 'Hello';
+console.log(char.charAt(0));
+console.log(char.charAt(1));
+
+// 4. filter() digunakan untuk membuat array baru dengan semua nilai yang lolos
+const price = [10, 15, 20, 30, 35, 40]
+const filterPrice = price.filter((price) => price > 25)
+console.log(filterPrice)
+
+// 5. find() digunakan untuk mengembalikan nilai dari elemen pertama dalam array yang lolos
+const num = [5, 10, 15];
+const checkNum = num.find((num) => num > 12);
+console.log(checkNum);
+
+// 6. some() digunakan untuk mengembalikan nilai true jika setidaknya satu elemen dalam larik memenuhi fungsi pengujian yang disediakan
+const age = [15, 16, 17, 18, 19];
+const checkAge = age.some((age) => age < 17);
+console.log(checkAge);
+
+// 7. repeat() digunakan untuk mengulangi sebuah string
+const string = ' Hai !';
+console.log(string.repeat(2));
+
+// 8. sort() digunakan mengurutkan elemen dalam sebuah array
+const no = [20, 13, 17, 1, 90];
+console.log(no.sort());
---
+++ Javascript-intro2/task2.js
@@ -0,0 +1,29 @@
+// Program searching nama
+const names = [
+    'Abigail', 'Alexandra', 'Alison',
+    'Amanda', 'Angela', 'Bella',
+    'Carol', 'Caroline', 'Carolyn',
+    'Deirdre', 'Diana', 'Elizabeth',
+    'Ella', 'Faith', 'Olivia', 'Penelope']
+
+
+function searchName(keywoard, limit, callback) {
+    //ubah keyword menjadi lowercase supaya menghiraukan case sensitif
+    keywoard = keywoard.toLowerCase();
+    //menggunakan method filter untuk mengetahui nama yang menggandung huruf tertentu dan menghiraukan case sensitif
+    let filterName = names.filter(name => name.toLowerCase().indexOf(keywoard) !== -1)
+    filterName.length = limit
+    callback(filterName);
+}
+
+function callback(filterName) {
+    //menghapus nilai kosong dari arrray
+    let emptyName = filterName.filter(nama => nama != null);
+    if (emptyName.length == 0) {
+        console.log('nama tidak ada')
+    } else {
+        console.log(emptyName)
+    }
+}
+
+searchName('an', 3, callback)
